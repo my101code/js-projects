e86927bb6037a0d215a2948efa3e8b9905c3d2ee
Moha Lhabdellaui <my101code@gmail.com>
2025-06-25 20:06:06 +0100

    add js 005

---
+++ 005-js-getElementById-onclick.php
@@ -0,0 +1,13 @@
+<?php include('header.php')?>
+<!-- ------------------------------ -->
+
+<button id="miBoton">Botón</button>
+
+<script>
+  document.getElementById("miBoton").onclick = function() {
+    alert("Botón clickeado");
+  };
+</script>
+
+<!-- ------------------------------ -->
+<?php include('footer.php')?>
