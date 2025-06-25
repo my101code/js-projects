a800cb03c5304c88c2449da18864875049f0e41a
Moha Lhabdellaui <my101code@gmail.com>
2025-06-25 20:15:36 +0100

    add js 006

---
+++ 006-js-addEventListener-click.php
@@ -0,0 +1,12 @@
+<?php include('header.php')?>
+<!-- ------------------------------ -->
+
+<button id="miBoton">Botón</button>
+<script>
+    document.getElementById("miBoton").addEventListener("click", function() {
+      console.log("Click detectado");
+    });
+</script>
+
+<!-- ------------------------------ -->
+<?php include('footer.php')?>
