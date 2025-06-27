68c6633c9336c9da24c58dc374fe414699becf29
Moha Lhabdellaui <my101code@gmail.com>
2025-06-27 13:39:27 +0100

    update  003

---
+++ json/003-cargar-json-externo-empleados.php
@@ -0,0 +1,25 @@
+<?php include('../header.php')?>
+<!-- ------------------------------ -->
+
+<script>
+
+// Suponiendo que tenemos un archivo "datos.json" en el servidor:
+fetch('empleados.json')
+  .then(response => response.json()) // Convierte la respuesta a JSON
+  .then(data => {
+    console.log(data); // Objeto JavaScript con los datos
+    // Ejemplo: Mostrar datos en el html
+    // document.getElementById("nombre").textContent = data.nombre;
+
+    // llanada a toda la fila
+    console.log(data.employees[1]);
+
+    // llamada a un componente de una fila
+    console.log(data.employees[1]["firstName"]);
+  })
+  .catch(error => console.error("Error al cargar el JSON:", error));
+
+</script>
+
+<!-- ------------------------------ -->
+<?php include('../footer.php')?>
